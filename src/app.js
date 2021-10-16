9ed945c7097348de23b239416b4ce036f99cbde9
karapetyansatenik <karapetyansatenik99@gmail.com>
2021-10-16 19:58:14 +0400

    Register and Login Full Lifecycle with NodeJS

---
+++ src/app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.API_PORT || 4444;
+
+const bcrypt = require("bcrypt");
+const saltRounds = 13;
+
+const users = require("./data").users;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async(req, res) => {
+    let userMatch = users.find((user) => req.body.email === user.email);
+    if (!userMatch) {
+        //TODO: hash the password with bcrypt.hash()
+        let passHash = await bcrypt.hash(req.body.password, saltRounds);
+        let newUser = {
+            _id: Date.now(),
+            email: req.body.email,
+            password: passHash,
+        };
+        users.push(newUser);
+        console.log("Full user list", users);
+        res.status(201).send({ data: newUser });
+    } else {
+        res
+            .status(400)
+            .send({ error: { code: 400, message: "Email address already used" } });
+    }
+});
+
+app.post("/login", async(req, res) => {
+    let userMatch = users.find((user) => req.body.email === user.email);
+    let submittedPass = req.body.password;
+    let savedPass = userMatch.password;
+    if (userMatch) {
+        //TODO: use bcrypt.compare() to actually hash and compare the password
+        const passwordDidMatch = await bcrypt.compare(submittedPass, savedPass);
+        if (passwordDidMatch) {
+            res.status(200).send({ data: { token: "this is a pretend token" } });
+        } else {
+            res.status(401).send({
+                error: { code: 401, message: "invalid username or password." },
+            });
+        }
+    } else {
+        //cause a delay to hide the fact that there was no match
+        let fakePass = `$2b$${saltRounds}$invalidusernameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`;
+        await bcrypt.compare(submittedPass, fakePass);
+        //to slow down the process
+        res
+            .status(401)
+            .send({ error: { code: 401, message: "invalid username or password." } });
+    }
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.error("Failure to launch server");
+        return;
+    }
+    console.log(`Listening on port ${port}`);
+});
